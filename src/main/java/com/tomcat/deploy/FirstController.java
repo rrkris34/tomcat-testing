978a5edb78792f92157773f99d6ab09f5c898624
rrkris34 <rrkris34@hotmail.com>
2021-06-30 12:33:53 +0530

    Updated webcontent new

--- src/main/java/com/tomcat/deploy/FirstController.java
+++ src/main/java/com/tomcat/deploy/FirstController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class FirstController {
-	String n[]= {"James","Sharan","Hari","Ramesh","Vijay","Suresh"};
+	String n[]= {"James","Sharan","Hari","Ramesh","Vijay","Suresh","Raghavendra"};
   @GetMapping("/greet")
   public String greet() {
 	  return "Hi!!! Spring boot is running - Jenkin webhook testing";
